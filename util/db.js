5e1152835738e78c479383d36708d503054210ac
Botond Kulcsar <bkulcsar@gmx.com>
2022-01-26 22:57:19 +0200

    exercises 13.17-13.18.

--- util/db.js
+++ util/db.js
@@ -1,5 +1,6 @@
 const { Sequelize } = require('sequelize')
 const { DATABASE_URL } = require('./config')
+const { Umzug, SequelizeStorage } = require('umzug')
 
 const sequelize = new Sequelize(DATABASE_URL, {
   dialectOptions: {
@@ -10,9 +11,37 @@ const sequelize = new Sequelize(DATABASE_URL, {
   }
 })
 
+const migrationConfig = {
+  migrations: { glob: 'migrations/*js' },
+  storage: new SequelizeStorage({ sequelize, tableName: 'migrations' }),
+  context: sequelize.getQueryInterface(),
+  loggger: console
+}
+
+const runMigrations = async () => {
+  const migrator = new Umzug(migrationConfig)
+  const migrations = await migrator.up()
+
+  console.log('Migrations up to date', {
+    files: migrations.map(mig => mig.name)
+  })
+}
+
+const rollbackMigrations = async () => {
+  await sequelize.authenticate()
+  const migrator = new Umzug(migrationConfig)
+  const migrations = await migrator.down()
+  console.log('Migrations rolled back', {
+    files: migrations.map(mig => mig.name)
+  })
+}
+
 const connectToDatabase = async () => {
   try {
     await sequelize.authenticate()
+
+    await runMigrations()
+
     console.log('Database connected')
   } catch (error) {
     console.log('DB connection FAILED! Error: ', error)
@@ -22,4 +51,4 @@ const connectToDatabase = async () => {
   return null
 }
 
-module.exports = { sequelize, connectToDatabase }
+module.exports = { sequelize, connectToDatabase, rollbackMigrations }
